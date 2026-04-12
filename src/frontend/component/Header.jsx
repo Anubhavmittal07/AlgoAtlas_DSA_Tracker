33375ddef37edc4c8ba6ffd93725c5ed1a9afe06
Arnav srivastava <srivastavaarnav26@gmail.com>
2026-04-13 01:54:46 +0530

    Added About

--- src/frontend/component/Header.jsx
+++ src/frontend/component/Header.jsx
@@ -1,12 +1,44 @@
 import { useEffect } from 'react'
 import React, { useContext } from 'react'
 import { ThemeContext } from './ThemeProvider'
+<<<<<<< HEAD
 import logo from '../../assets/unnamed.jpg'
 import { Link } from 'react-router-dom';
+=======
+import logo from '../../assets/logo2.0.jpeg'
+import { Link, useLocation, useNavigate } from 'react-router-dom';
+>>>>>>> 2c5b40f (Added About)
 
 const Header = () => {
 
   const { dark, setDark } = useContext(ThemeContext);
+  const location = useLocation();
+  const navigate = useNavigate();
+
+  // Scroll to #about when hash changes
+  useEffect(() => {
+    if (location.hash === '#about') {
+      setTimeout(() => {
+        const el = document.getElementById('about');
+        if (el) {
+          el.scrollIntoView({ behavior: 'smooth' });
+        }
+      }, 100);
+    }
+  }, [location]);
+
+  const handleAboutClick = () => {
+    if (location.pathname === '/') {
+      // Already on home page, just scroll
+      const el = document.getElementById('about');
+      if (el) {
+        el.scrollIntoView({ behavior: 'smooth' });
+      }
+    } else {
+      // Navigate to home page with hash
+      navigate('/#about');
+    }
+  };
 
   console.log(dark);
   return (
@@ -37,8 +69,13 @@ const Header = () => {
             Wishlist
           </div>
         </Link>
+<<<<<<< HEAD
         <div className="about cursor-pointer hover:text-red-500">About</div>
         {/* <div className="mode cursor-pointer  hover:text-red-500">
+=======
+        <div className="about cursor-pointer hover:text-red-500" onClick={handleAboutClick}>About</div>
+        <div className="mode cursor-pointer  hover:text-red-500">
+>>>>>>> 2c5b40f (Added About)
           {dark ? <span onClick={() => setDark(false)}>Light</span> : <span onClick={() => setDark(true)}>Dark</span>}
         </div> */}
         <div 

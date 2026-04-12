ad1e8a1d956d78f8d514569dd067b97c543c83b4
Anubhav <anubhavmittal0701@gmail.com>
2026-04-13 01:34:51 +0530

    added home

--- src/frontend/component/Header.jsx
+++ src/frontend/component/Header.jsx
@@ -1,7 +1,7 @@
 import { useEffect } from 'react'
 import React, { useContext } from 'react'
 import { ThemeContext } from './ThemeProvider'
-import logo from '../../assets/logo2.0.jpeg'
+import logo from '../../assets/unnamed.jpg'
 import { Link } from 'react-router-dom';
 
 const Header = () => {
@@ -12,22 +12,42 @@ const Header = () => {
   return (
     <div className='h-16 w-full text-var(--navbar-color) font-bold flex items-center justify-between  space-x-4 border-b-2 ' style={{ background: 'var(--navbar-bg' }}>
       <div className="logo ml-5 ">
-        <img src={logo} height={50} width={60} className='rounded-[25px]' />
-      </div>
+        <Link to="/">
+          <img 
+            src={logo} 
+            height={60} 
+            width={60} 
+            className='rounded-[25px] cursor-pointer' 
+          />
+        </Link>
+        {/* <img src={logo} height={50} width={60} className='rounded-[25px]' />*/}
+      </div> 
 
       {/* //header */}
+      
+
       <div className="flex mr-5 space-x-4">
+        <Link to="/">
+        <div className="home cursor-pointer hover:text-red-500">
+          Home
+        </div>
+      </Link>
         <Link to="/favour">
           <div className="add cursor-pointer hover:text-red-500">
             Wishlist
           </div>
         </Link>
         <div className="about cursor-pointer hover:text-red-500">About</div>
-        <div className="mode cursor-pointer  hover:text-red-500">
+        {/* <div className="mode cursor-pointer  hover:text-red-500">
           {dark ? <span onClick={() => setDark(false)}>Light</span> : <span onClick={() => setDark(true)}>Dark</span>}
+        </div> */}
+        <div 
+          className="mode cursor-pointer hover:text-red-500"
+          onClick={() => setDark(!dark)} >
+          Theme
         </div>
-        <div className="login cursor-pointer  hover:text-red-500">Login</div>
-        <div className="div cursor-pointer hover:text-red-500">SignUp</div>
+        <div className="login cursor-pointer  hover:text-red-500">Logout</div>
+        {/* <div className="div cursor-pointer hover:text-red-500">SignUp</div> */}
       </div>
         
     </div>
---
+++ src/frontend/component/auth.jsx
@@ -0,0 +1,5 @@
+export const users=[
+    { username: "anubhav", password: "1234" },
+  { username: "test", password: "test123" },
+  { username: "admin", password: "admin" }
+];
